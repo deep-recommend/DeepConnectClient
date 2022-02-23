189b61d1cf765e3f57b62377a7a6436b39ccdc3d
deep-recommend-sugimoto <deeprecommend@gmail.com>
2022-02-24 08:00:23 +0900

    message-room resolver

--- src/app/pages/message-room-pages/message-room/message-room.resolver.ts
+++ src/app/pages/message-room-pages/message-room/message-room.resolver.ts
@@ -4,14 +4,27 @@ import {
     Resolve,
     RouterStateSnapshot,
 } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { UiStore } from 'src/app/states/ui/ui.store';
+import { MessageService } from '../../../states/message/message.service';
 
 @Injectable()
-export class MessageRoomResolverService implements Resolve<void> {
-    constructor(private readonly uiStore: UiStore) {}
+export class MessageRoomResolverService implements Resolve<Observable<void>> {
+    constructor(
+        private readonly uiStore: UiStore,
+        private readonly messageService: MessageService
+    ) {}
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): void {
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<void> {
         this.uiStore.hideRoutingTab();
         this.uiStore.displayPageName(route.data.title);
+
+        return forkJoin(this.messageService.getMessagesRequest()).pipe(
+            map((observer) => void observer)
+        );
     }
 }
